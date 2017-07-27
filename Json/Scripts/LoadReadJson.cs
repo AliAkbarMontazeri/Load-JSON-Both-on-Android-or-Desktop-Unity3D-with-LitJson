a4398bcf79b6c14953c5d7222b1bd1cf824e6fe5
AliAkbarMontazeri <destroyerick@gmail.com>
2017-07-27 13:58:33 +0700

    Add files via upload

---
+++ Json/Scripts/LoadReadJson.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+using System.IO;
+
+public class LoadReadJson : MonoBehaviour {
+
+    private List<ValueTest> _database = new List<ValueTest>();
+    private JsonData _itemData;
+
+    public int valueItem;
+
+    void Start() {
+        //if(Application.platform == RuntimePlatform.Android){
+        //    //StartCoroutine("Mobile");
+        //    ReadJSONMobile();
+        //}
+        //else if(Application.isEditor){
+        //    if(File.Exists(Application.streamingAssetsPath + "/ValueTest.json")){
+        //        Debug.Log("Ada");
+        //    }
+        //    _itemData = JsonMapper.ToObject(File.ReadAllText(Application.streamingAssetsPath + "/ValueTest.json"));
+        //}
+        ReadJSONMobile();
+        ConstructItemDatabase();
+        Debug.Log(valueItem);
+    }
+
+    IEnumerator Mobile() {
+        WWW loadDb = new WWW(Application.streamingAssetsPath + "/ValueTest.json");
+        yield return loadDb;
+        File.WriteAllBytes(Application.persistentDataPath + "/ValueTest.json", loadDb.bytes);
+        _itemData = JsonMapper.ToObject(File.ReadAllText(Application.persistentDataPath + "/ValueTest.json"));
+    }
+
+    void ReadJSONMobile() {
+        TextAsset file = Resources.Load("ValueTest") as TextAsset;
+        string content = file.ToString();
+        _itemData = JsonMapper.ToObject(content);
+    }
+
+    void ConstructItemDatabase() { 
+        for(int i = 0; i < _itemData.Count; i++){
+            _database.Add(new ValueTest(
+                (int)_itemData[i]["id"], _itemData[i]["title"].ToString(), (int)_itemData[i]["value"]));
+        }
+        valueItem = _itemData.Count;
+    }
+
+    public ValueTest FetchItemByID(int id) {
+        for(int i = 0; i < _database.Count; i++){
+            if(_database[i].Id == id){
+                return _database[i];
+            }
+        }
+        return null;
+    }
+}
+
+public class ValueTest{
+    
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Value { get; set; }
+
+    public ValueTest(int id, string title1, int value1) {
+        this.Id = id;
+        this.Title = title1;
+        this.Value = value1;
+    }
+
+    public ValueTest() { 
+    
+    }
+}
---
+++ Json/Scripts/ShowData.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowData : MonoBehaviour {
+
+    public Text[] showTitle = new Text[4];
+    public Text[] showValue = new Text[4];
+
+    LoadReadJson _database;
+
+    void Start() {
+        _database = GetComponent<LoadReadJson>();
+    }
+
+    public void DoShow() { 
+        for (int i = 0; i < _database.valueItem; i++){
+            Show(i);
+        }
+    }
+
+    void Show(int id) {
+        ValueTest valueToAdd = _database.FetchItemByID(id);
+        
+        switch(id){
+            case 0:
+                showTitle[id].text = valueToAdd.Title;
+                showValue[id].text = valueToAdd.Value.ToString();
+                break;
+            case 1:
+                showTitle[id].text = valueToAdd.Title;
+                showValue[id].text = valueToAdd.Value.ToString();
+                break;
+            case 2:
+                showTitle[id].text = valueToAdd.Title;
+                showValue[id].text = valueToAdd.Value.ToString();
+                break;
+            case 3:
+                showTitle[id].text = valueToAdd.Title;
+                showValue[id].text = valueToAdd.Value.ToString();
+                break;
+        }
+    }
+}
